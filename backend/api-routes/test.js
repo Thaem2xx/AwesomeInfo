6ea8c93f042f7995b1f2c93df51b1840d01e7e4a
thiem <hcaothiem@gmail.com>
2019-12-05 15:56:45 +0700

    Call RestAPI

--- backend/api-routes/test.js
+++ backend/api-routes/test.js
@@ -1,5 +1,11 @@
 // Initialize express router
 let router = require('express').Router();
+router.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 // Set default API response
 router.get('/', function (req, res) {
     res.json({
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
+// const cors = require('cors');
 var app = express();
-const port = 8080;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// app.use(cors());
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
@@ -10,12 +15,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 let apiRoutes = require("./api-routes/test")
 
+
 // Use Api routes in the App
 app.use('/api', apiRoutes)
 
 //database
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/restAPI',{ useNewUrlParser: true});
+mongoose.connect(process.env.DB_LOCALHOST,{ useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
